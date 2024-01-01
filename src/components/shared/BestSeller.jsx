0e6f19c4734911a8898abafad83c43300cb0ec89
Negar <negar.karimnejad@gmail.com>
2024-01-01 15:46:28 +0330

    coding

--- src/components/shared/BestSeller.jsx
+++ src/components/shared/BestSeller.jsx
@@ -8,15 +8,17 @@ export default function BestSeller() {
       <Image
         className="absolute left-0 top-[350px] -z-10"
         src="/sallad1.png"
-        width={115}
-        height={115}
+        width={100}
+        height={100}
+        style={{ width: "20%", height: "auto" }}
         alt="sallad"
       />
       <Image
         className="absolute right-0 top-[350px] -z-10"
         src="/sallad2.png"
-        width={115}
-        height={115}
+        width={100}
+        height={100}
+        style={{ width: "20%", height: "auto" }}
         alt="sallad"
       />
       <SectionHeader smallTitle="Check out" mainTitle="Our Best Sellers" />

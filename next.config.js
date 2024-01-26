67b9b0bb0d7be60630f76d25d17f06682f110483
negar <negar.karimnejad@gmail.com>
2024-01-26 21:09:49 +0330

    coding

--- next.config.js
+++ next.config.js
@@ -25,5 +25,5 @@ const nextConfig = {
 module.exports = {
   // time in seconds of no pages generating during static
   // generation before timing out
-  staticPageGenerationTimeout: 1000,
+  staticPageGenerationTimeout: 4000,
 };
